a3658044ffab0ddaec812da0214709979fc0576e
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-03-04 03:22:39 +0200

    Update 2-hbnb.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+        if (textStatus === 'success') {
+            if (data.status === 'OK') {
+                $('#api_status').addClass('available');
+            } else {
+                $('#api_status').removeClass('available');
+            }
+        }
+    });
+});
